e55d593b2c5b12554e1c37661cee24292005f734
Arunav Gupta <centaurus.ab@gmail.com>
2020-06-04 19:39:55 -0700

    setup

---
+++ sigmet.py
@@ -0,0 +1,11 @@
+
+
+class Sigmet:
+
+    def __init__(start_point, end_point, data):
+        this.start_point = start_point
+        this.end_point = end_point
+        this.data = data
+
+    def fit(cutoff_date, p, q, d):
+        pass
