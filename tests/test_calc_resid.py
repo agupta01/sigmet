b8c8b7367565c35199a11cb1c049f7cd83011bba
Arunav Gupta <centaurus.ab@gmail.com>
2020-06-25 12:45:18 -0700

    rewrite tests with new architecture

--- tests/test_calc_resid.py
+++ tests/test_calc_resid.py
@@ -1,37 +1,30 @@
 """
 This file tests the calculate residual helper function, used in .fit().
 """
-import sys
 import numpy as np
+import pandas as pd
 
-sys.path.insert(1, '../')
-import sigmet
+import sigmet.au3_functions as au3
 
-data_regular = np.array([0, -2, -3, -2, -1, 0])
-reg_regular = np.array([0, 1, 1, 0, 1, 0])
-data_flat = np.array([100, 100, 100, 100, 100])
-reg_flat = np.array([100.1, 100.1, 100.1, 100.1, 100.1])
+dates = pd.date_range(start='1/1/2005', periods=12, freq='M')
 
-def reset_data():
-    """
-    Resets data to original state, in case it's been modified during a test.
-    """
-    global data_regular, reg_regular, data_flat, reg_flat
-    data_regular = np.array([0, -2, -3, -2, -1, 0])
-    reg_regular = np.array([0, 1, 1, 0, 1, 0])
-    data_flat = np.array([100, 100, 100, 100, 100])
-    reg_flat = np.array([100.1, 100.1, 100.1, 100.1, 100.1])
+data_regular = pd.Series(data=[0, -2, -3, -2, -1, 0, 1, 1, 3, -2, -4, -9], index=dates)
+reg_regular = pd.Series(data=[0, 1, 1, 2, 1, 0, 1, 2, 3, 4, 5, 7], index=dates)
+data_flat = pd.Series(data=np.repeat(100, 12), index=dates)
+reg_flat = pd.Series(data=np.repeat(100.1, 12), index=dates)
 
 def test_regular():
     """
     Tests a 'normal' regression, no irregular cases.
     """
-    s = Sigmet(data_regular)
-    assert 11 == s.calc_resid(data_regular, reg_regular)
+    assert 13 == au3.calc_resid(data_regular, reg_regular,
+                                                start_date=pd.to_datetime('1/31/2005'),
+                                                end_date=pd.to_datetime('6/30/2005'))
 
 def test_flat():
     """
-    Tests a 'flat' trendline.
+    Tests a 'flat' regression, where end date is at the end of the Series.
     """
-    s = Sigmet(data_flat)
-    assert 0.5 == s.calc_resid(data_flat, reg_flat)
+    assert 1.2 == au3.calc_resid(data_flat, reg_flat,
+                                                start_date=pd.to_datetime('1/31/2005'),
+                                                end_date=pd.to_datetime('12/31/2005'))
