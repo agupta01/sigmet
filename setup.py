ee5da0553e7d29fae1129b4cd01666c368f2b4ed
kmc026 <kmc026@ucsd.edu>
2020-07-09 18:03:51 -0700

    standardize, setup

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+from io import open as io_open
+
+source_directory = os.path.abspath(os.path.dirname(__file__))
+requirements_text = os.path.join(src_dir, 'requirements.txt')
+with io_open(requirements_text, mode='r') as fd:
+    install_requires = [i.strip() for i in fd.read().strip().split('\n')]
+
+#here = path.abspath(path.dirname(__file__))
+#with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "sigmet",
+    version = "0.0.1",
+    author = "Zillow Team",
+    author_email = "author@example.com",
+    description = "A tool that measures shocks",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/agupta01/sigmet",
+    packages = find_packages('sigmet'),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: GNU GENERAL PUBLIC LICENSE",
+        "Operating System :: OS Independent (Linux in progress)",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: UCSD",
+        "Topic :: Software Development :: Build Tools",
+    ],
+    python_requires = '>=3.6',
+    install_requires = install_requires,
+#    extras_require={  # Optional
+#        'dev': ['check-manifest'],
+#        'test': ['coverage'],
+#    }
+    package_data={
+        'sigmet': ['README.md', 'LICENCE', 'requirements.txt'],
+    }
+)
+
--- sigmet/sigmet.py
+++ sigmet/sigmet.py
@@ -1,5 +1,3 @@
-
-
 class Sigmet:
 
     def __init__(start_point, end_point, data):
@@ -7,5 +5,75 @@ class Sigmet:
         this.end_point = end_point
         this.data = data
 
-    def fit(cutoff_date, p, q, d):
-        pass
+
+
+    def fit(self, cutoff_date, p, q, d, standardize=False):
+    """
+    Fits the pandas.Series object
+    
+    Parameters
+    __________
+    
+    cutoff_date : 
+    
+    p :
+    
+    q :
+    
+    d :
+    
+    standardize : boolean object, default=False
+        If False, then no change to the fitted Series.
+        If True, then the fitted Series will be standardized before being passed into .fit() .
+        
+    Returns
+    _______
+    
+    self : object
+        Returns an instance of self.
+    """
+
+    srs = self.copy(deep=True)
+    
+    temp = standardize_series(srs, standardize)
+
+
+
+    return self
+    
+    
+    
+    def standardize_series(series, standardize):
+        """
+        Standardizes the pandas.Series object
+        
+        Parameters
+        __________
+        
+        series : pandas.Series object
+            A pandas.Series object.
+            
+        standardize : boolean object, default=False
+            If False, then no change to the fitted Series.
+            If True, then the fitted Series will be standardized before being passed into .fit() .
+            
+        Return
+        ______
+        series : pandas.Series object
+            Returns the original fitted Series.
+        
+        result : pandas.Series object
+            Returns a standardized Series.
+        """
+        
+        if standardize == True:
+            
+            mu = series.mean()
+            sigma = series.std()
+        
+            result = series.subtract(mu).divide(sigma)
+            return result
+            
+        else:
+            
+            return series
