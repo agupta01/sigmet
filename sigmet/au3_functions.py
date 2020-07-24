1a9e9959ee487d9b23b2dd70d85fc05365b968cf
Luke Nguyen <Lucas Nguyen>
2020-07-23 17:54:24 -0700

    Add find end baseline, ARIMA tests

--- sigmet/au3_functions.py
+++ sigmet/au3_functions.py
@@ -7,8 +7,8 @@ This module accepts a pandas Series and assumes that it contains a DateTime
 Index.
 
 This script requires that `pandas`, `numpy`, `tqdm`, `matplotlib`, `sklearn`,
-`datetime`, `statsmodels`, and `scipy` be installed within the Python
-environment within which you are running this script.
+`statsmodels`, and `scipy` be installed within the Python environment within
+which you are running this script.
 
 This file can be imported as a module and contains the following functions:
 
@@ -24,12 +24,11 @@ This file can be imported as a module and contains the following functions:
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime
 from tqdm import tqdm
-from sklearn.preprocessing import normalize
 from sklearn.preprocessing import StandardScaler
 from statsmodels.tsa.arima_model import ARIMA
-from scipy.signal import argrelextrema
+import statsmodels.api as sm
+from scipy.signal import argrelextrema, argrelmax
 
 plt.style.use("seaborn")
 
@@ -57,7 +56,7 @@ def standardize(series):
     return scaler.fit_transform(train)
 
 
-def find_start(series, start_date, end_date, threshold):
+def find_start(series, user_start, user_end, ma_window=6):
     """Gets start date of dip in TS, measured as the largest local maximum for
     a given element in the database
 
@@ -78,127 +77,38 @@ def find_start(series, start_date, end_date, threshold):
         The start date
     """
 
-    [0, 0, 0, 0, 0, 0, 0]
-    [0, 0, 0, 0, 0, 0]
-    [0, 0, 0, 4, 4, 4, 0, 0]
-    [0, 0, 4, 0, 0, -4, 0]
-
-    # Differencing threshold
-    THRESHOLD = 1
-
     # Get the most recent date and filter series
-    series = series[series.index < end_date]
-    series = series[1:]
+    series_filtered = series[(
+        series.index >= user_start) & (
+            series.index <= user_end)]
 
-    # Initialize differences array for maxes
-    diffs = series.copy()
+    ser_smoothed = series_filtered.rolling(ma_window).mean()
+    max_indices = argrelmax(ser_smoothed.values)[0]
+    max_indices_not_smoothed = argrelmax(series_filtered.values)[0]
 
-    # Populate is_max array using differencing
-    is_max = np.array([((diffs[i] >= 0) and (diffs[i + 1] < 0)) or 
-                       ((diffs[i] > 0) and (diffs[i + 1] <= 0)) and (
-                (diffs[i + 1] - diffs[i]) <= THRESHOLD)
-        for i in range(len(diffs) - 1)])
-
-    is_max = np.append(is_max, False)
-
-    # If there are no local maxes, return the start date
-    if np.count_nonzero(is_max) == 0:
-        return start_date
-
-    maxes = is_max
-
-    # Get the proper minimum date
-    recession_minimum_date = my_min(series, start_date, end_date, threshold)
-    print(recession_minimum_date)
-    # Get max before that date
-    if len(maxes) != 0:
-        for i in range(len(series)):
-            if ((maxes[i]) and (series.index[i] < recession_minimum_date)):
-                max_index = i
-        
-        print(max_index)
-        start_date = series.index[max_index]
-
-    # Get the actual start date
-    return start_date
+    if (len(max_indices) > 0):
+        first_max_index = max_indices[0]
+        global_min = ser_smoothed[first_max_index:].min()
+        global_min_index = ser_smoothed.values[first_max_index:].argmin()
+        valid_max_indices = list(
+            max_indices[(
+                max_indices >= first_max_index) & (
+                    max_indices < global_min_index + first_max_index)])
+        max_idx = list(ser_smoothed[valid_max_indices].values - global_min)
+        max_idx = max_idx.index(min(max_idx)) + first_max_index
+        maxes_not_smoothed = list(max_indices_not_smoothed[
+            :(max_idx + ma_window)])
+        compare = list(abs(maxes_not_smoothed - max_idx))
+        final_max_index = maxes_not_smoothed[compare.index(min(compare))]
+        return series_filtered.index[final_max_index]
+    elif (len(max_indices_not_smoothed) > 0):
+        return series_filtered.index[max_indices_not_smoothed[0]]
+    return user_start
 
 
-def minus_helper(col, x):
-    """Subtracts two elements in a series
 
-    Parameters
-    ----------
-    col : pd.Series
-        The input column
-    x : int
-        The location
-
-    Returns
-    -------
-    int
-        The result
-    """
-
-    return col.iloc[x] - col.iloc[x - 1]
-
-
-def my_min(series, start_date, end_date, threshold=-0.002):
-    """Finds an appropriate local minimum to detect a dip accurately, uses
-    differencing
-
-    Parameters
-    ----------
-    series : pd.Series
-        Find the correct min within a given time series
-    start_date : pd.DateTime
-        The start date
-    end_date : pd.DateTime
-        The end date
-    threshold : int
-        The differencing threshold for finding the correct min
-
-    Returns
-    -------
-    str
-        The date at which the correct minimum occurs
-    """
-
-    # Filter series
-    series = series.where(
-        (series.index > start_date) & (series.index < end_date))
-    my_feature = series.to_numpy()
-
-    # Get min indeces by finding local mins in the numpy array and min vals
-    min_indeces = argrelextrema(my_feature, np.less)[0]
-    min_vals = [my_feature[val] for val in min_indeces]
-
-    # Get the array of dates at which the minima occur
-    min_dates = [series.index.to_numpy()[val] for val in min_indeces]
-
-    # For iteration, update this current index if differencing threshold is
-    # reached
-    curr_index = 0
-
-    for i in range(len(min_vals)):
-        # If we reached the end
-        if i == len(min_vals) - 1:
-            curr_index = len(min_vals) - 1
-            break
-        else:
-            # Check differencing threshold
-            if np.diff([min_vals[i], min_vals[i + 1]]) <= threshold:
-                curr_index += 1
-            else:
-                curr_index = i
-                break
-    
-    if min_vals:
-        return min_dates[curr_index]
-    return end_date
-
-
-def ARIMA_50(series, start_date, params=(5, 1, 1)):
-    """Get an ARIMA forecast for a given Series
+def SARIMAX_50(series, start_date, params=(5, 1, 1)):
+    """Get an SARIMAX forecast for a given Series
 
     Parameters
     ----------
@@ -207,7 +117,7 @@ def ARIMA_50(series, start_date, params=(5, 1, 1)):
     start_date : pd.DateTime
         DateTime object from index of df representing peak feature
     params : tuple
-        p, d, and q parameters for ARIMA
+        p, d, and q parameters for SARIMAX
 
     Returns
     -------
@@ -215,28 +125,30 @@ def ARIMA_50(series, start_date, params=(5, 1, 1)):
         Series of forecasts
     """
 
-    try:
-        # Filter series
-        before = series.where(series.index >= start_date)
+    #try:
+    # Filter series
+    before = series[series.index >= start_date]
+    before.dropna(inplace=True)
 
-        # Steps for ARIMA forecast
-        steps = before.values.shape[0]
+    # Steps for ARIMA forecast
+    steps = before.values.shape[0]
 
-        # Initialize model
-        model = ARIMA(before, order=(5, 1, 1))
+    # Initialize model
+    model = sm.tsa.statespace.SARIMAX(before, trend='c', order=params)
+        
+    # Fit the model
+    model_fit = model.fit(disp=0)
 
-        # Fit the model
-        model_fit = model.fit(disp=0)
-
-        # Return the forecast as a pd.Series object
-        return pd.Series(model_fit.forecast(steps)[0])
-    except ValueError:
-        raise "Cannot provide an ARIMA forecast for given trend"
+    # Return the forecast as a pd.Series object
+    return pd.Series(model_fit.forecast(steps)[0])
+    #except ValueError:
+    #    raise ValueError("Cannot provide an SARIMAX forecast for given trend")
 
 
-def find_end(series, start_date, ARIMA_50):
-    """Gets end date of dip in TS, measured as the first point of intersection
-    between feature trend and ARIMA_50 foreast for a given element
+def find_end_forecast(series, start_date, SARIMAX_50):
+    """
+    Gets end date of dip in TS, measured as the first point of intersection
+    between feature trend and SARIMAX_50 foreast for a given element
 
     Parameters
     ----------
@@ -244,8 +156,8 @@ def find_end(series, start_date, ARIMA_50):
         The input series in which to find the end date
     start_date: pd.datetime
         The start date of the dip
-    ARIMA_50 : pd.Series
-        ARIMA_50 forecast with which to measure the intersection
+    SARIMAX_50 : pd.Series
+        SARIMAX_50 forecast with which to measure the intersection
 
     Returns
     -------
@@ -253,18 +165,19 @@ def find_end(series, start_date, ARIMA_50):
         The end date of the dip in TS
     """
 
+   
     # Calculate differences, use a DataFrame to find the end
     series = series.where(series.index >= start_date)
-    diffs = ARIMA_50 - series.values
+    diffs = SARIMAX_50 - series.values
     residual_df = pd.DataFrame(data={
         'Date': series.index.values, 'Delta': diffs})
 
     # Filter only positive residuals, and most recent one is the last
     # recession date
     most_recent_positive_delta = residual_df[
-        residual_df['Delta'] > 0].sort_values('Date', ascending=False)
+        residual_df['Delta'] >= 0].sort_values('Date', ascending=False)
 
-    # If ARIMA model indicates a sharp drop, set end date as one month after
+    # If SARIMAX model indicates a sharp drop, set end date as one month after
     # start date
     if (most_recent_positive_delta.shape[0] == 0):
         return residual_df.Date.values[0]
@@ -272,6 +185,45 @@ def find_end(series, start_date, ARIMA_50):
     end_date = most_recent_positive_delta['Date'].iloc[0]
 
     return end_date
+
+
+def find_end_baseline(series, start_date, user_end):
+    """
+    Gets end date to stop calculating AU3, measured as the first point in 
+    time-series greater than max from find_start. If no point exists, return user end date
+
+    Parameters
+    ----------
+    series : pd.Series
+        The input series in which to find the end date
+    start_date: pd.datetime
+        Start date identified from find_start
+    user_end: pd.datetime
+        User specified cutoff to stop calculating AU3 on series
+
+    Returns
+    -------
+    pd.DateTime
+        The end date of the dip in TS 
+    """
+    # Index series from identified recession start date to user specified end date
+    series_filtered = series[(
+        series.index > start_date) & (
+            series.index <= user_end)]
+
+    # Get value at start date
+    start_value = series[series.index == start_date]
+
+    # Find all values greater than start value after minimum
+    recession_min_index = series_filtered[series_filtered == series_filtered.min()].index
+    series_after_min = series_filtered[series_filtered.index > recession_min_index]
+    positive_deltas = series_after_min[series_after_min >= start_value]
+    
+    # If no values greater than start return user end date, else return first value
+    if len(positive_deltas) == 0:
+        return user_end
+    return positive_deltas.index[0]  
+
 
 
 def calc_resid(series, predicted, start_date, end_date):
@@ -297,7 +249,8 @@ def calc_resid(series, predicted, start_date, end_date):
     """
 
     # Filter series
-    series = series.loc[(series.index >= start_date) & (series.index <= end_date)]
+    series = series.loc[(
+        series.index >= start_date) & (series.index <= end_date)]
 
     # End for filtering predicted
     end_index = len(series)
@@ -308,7 +261,7 @@ def calc_resid(series, predicted, start_date, end_date):
     return sum(diffs)
 
 
-def find_AU3(series, start_date, end_date, threshold):
+def find_AU3(series, start_date, cutoff_for_start, threshold):
     """Calculates the AU3 score, given by the area between the ARIMA curve
     and actual curve given by the trend in the series
 
@@ -330,7 +283,7 @@ def find_AU3(series, start_date, end_date, threshold):
         The AU3 score
     """
 
-    start = find_start(series, start_date, end_date, threshold)
+    start = find_start(series, start_date, cutoff_for_start, threshold)
     arima = ARIMA_50(series, start)
     end = find_end(series, start, arima)
     return calc_resid(series, arima, start, end)
--- tests/test_ARIMA.py
+++ tests/test_ARIMA.py
@@ -1,5 +1,5 @@
 """
-This file tests the find start date helper function, used in .fit().
+This file tests the ARIMA helper function, used in .fit().
 """
 import numpy as np
 import pandas as pd
@@ -7,29 +7,83 @@ import scipy.stats
 import sigmet.au3_functions as au3
 from statsmodels.tsa.arima_model import ARIMA
 
-dates = pd.date_range(start='1/1/2005', periods=12, freq='M')
-increasing = pd.Series(
-    data=[1, 2, 3, 4, 5, 6, 6, 7, 8, 10, 12, 15], index=dates)
-decreasing = pd.Series(
-    data=[3, 2, 1, 0, 0, -1, -2, -5, -6, -8, -9, -15], index=dates)
-two_peak_one = pd.Series(data=[0, 4, 0, -4, 0, 6, 0, -6, -5, -4, -2, 0], index=dates)
-two_peak_two = pd.Series(
-    data=[0, 2, -2, -6, -5, -7, -9, -8, -9, -8, -6, -5], index=dates)
-multiple_peaks = pd.Series(
-    data=[0, 4, 0, -4, 0, 2, 0, 6, 0, -6, -6, 0], index=dates)
+
+begin_up = list(range(-12, 0))
+begin_down = list(range(12, 0, -1))
+begin_flat = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+begins = [begin_up, begin_down, begin_flat]
+
+dates = pd.date_range(start='1/1/2005', periods=24, freq='M')
+
+random = list(np.random.rand(1, 12).flatten())
 
 
-def test_two_peaks():
+
+def prepend_begins(case):
     """
-    Tests two peak cases.
+    Adds different data before test case to represent data before start date
     """
-    ARIMA
-    assert dates[5] == au3.find_start(two_peak_one, dates[0], dates[-1], 0.001)
-    assert dates[1] == au3.find_start(two_peak_two, dates[0], dates[-1], 0.001)
+    prepended = []
+
+    for begin in begins:
+        data = begin.copy()
+        data.extend(case)
+        ser = pd.Series(data=data, index=dates)
+        prepended.append(ser)
+
+    return prepended
 
 
-def test_multiple_peaks():
+def test_index_after_start():
     """
-    Tests multiple peaks
+    Tests whether ARIMA forecasts data after start date
     """
-    assert dates[1] == au3.find_start(multiple_peaks, dates[0], dates[-1], 0.001)
+    for case in prepend_begins(random):
+        assert 12 == len(au3.ARIMA_50(case, dates[12]))
+
+
+def test_forecast_up():
+    """
+    Tests whether ARIMA forecast is positive given increasing training data.
+    Cases use clean and noisy data to train ARIMA forecast
+    """
+    basic_data = begin_up.copy()
+    basic_data.extend(random)
+    basic_up = pd.Series(data=basic_data, index=dates)
+
+    assert all(au3.ARIMA_50(basic_up, dates[12]) >= noisy_up[12])
+
+    noisy_data = [x + y for x, y in zip(begin_up, random)].extend(random)
+    noisy_data.extend(random)
+    noisy_up = pd.Series(data=noisy_data, index=dates)
+
+    assert all(au3.ARIMA_50(noisy_up, dates[12] >= noisy_up[12]))
+
+
+def test_forecast_down():
+    """
+    Tests whether ARIMA forecast is negative given decreasing training data
+    Cases use clean and noisy data to train ARIMA forecast
+    """
+    basic_data = begin_down.copy()
+    basic_data.extend(random)
+    basic_down = pd.Series(data=basic_data, index=dates)
+
+    assert all(au3.ARIMA_50(basic_up, dates[12]) >= noisy_up[12])
+
+    noisy_data = [x - y for x, y in zip(begin_down, random)]
+    noisy_data.extend(random)
+    noisy_down = pd.Series(data=noisy_data, index=dates)
+
+    assert all(au3.ARIMA_50(noisy_up, dates[12] <= noisy_up[12]))
+
+
+
+def test_forecast_flat():
+    """
+    Tests whether ARIMA forecast is flat provided flat training data
+    """
+    data = begin_flat
+    data.extend(begin_flat)
+    flat_series = pd.Series(data=data, index=dates)
+    assert all(au3.ARIMA_50(flat_series, dates[12]) == 0)
