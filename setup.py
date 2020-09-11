fd6200f89c504a72b25f258425b6e60ffe571c8f
Arunav Gupta <centaurus.ab@gmail.com>
2020-09-10 20:55:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,39 +7,38 @@ requirements_text = os.path.join(source_directory, 'requirements.txt')
 with io_open(requirements_text, mode='r') as fd:
     install_requires = [i.strip() for i in fd.read().strip().split('\n')]
 
-#here = path.abspath(path.dirname(__file__))
-#with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-#    long_description = f.read()
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name = "sigmet",
-    version = "0.0.1",
-    author = "Zillow Team",
-    author_email = "author@example.com",
-    description = "A tool that measures shocks",
+    version = "0.9.0",
+    author = "Arunav Gupta, Camille Dunning, Lucas Nguyen, Ka Ming Chan",
+    author_email = "arunavg@ucsd.edu, adunning@ucsd.edu, kmc026@ucsd.edu",
+    description = "A Python package to find and measure negative price shocks in financial time-series data",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/agupta01/sigmet",
     packages = find_packages('sigmet'),
     classifiers = [
         "Programming Language :: Python :: 3",
-        "License :: GNU GENERAL PUBLIC LICENSE",
+        "License :: GNU GENERAL PUBLIC LICENSE v3",
         "Operating System :: OS Independent (Linux in progress)",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: UCSD",
-        "Topic :: Software Development :: Build Tools",
+        "Development Status :: 4 - Beta",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        "Topic :: Software Development :: Python Modules",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Information Analysis"
     ],
     python_requires = '>=3.6',
     install_requires = install_requires,
-#    extras_require={  # Optional
-#        'dev': ['check-manifest'],
-#        'test': ['coverage'],
-#    }
     package_data={
-        'sigmet': ['README.md', 'LICENCE', 'requirements.txt'],
+        'sigmet': ['README.md', 'LICENSE', 'requirements.txt'],
     }
 )
 
