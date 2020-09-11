b9299bdf420b38b0890b02bded78be68ce545277
Arunav Gupta <centaurus.ab@gmail.com>
2020-09-10 20:41:20 -0700

    finalize .graph() (#30)
    * Camille: Add graph
    
    * update .graph()

--- sigmet/sigmet.py
+++ sigmet/sigmet.py
@@ -1,6 +1,8 @@
 from .au3_functions import find_start, SARIMAX_predictor, find_end_baseline, calc_resid
 import seaborn as sns
 import matplotlib.pyplot as plt
+import warnings
+import pandas as pd
 
 class Sigmet:
 
@@ -50,16 +52,16 @@ class Sigmet:
             srs = standardize(srs)
 
         if not force_start:
-            start = find_start(srs, window_start, window_end, ma_window=moving_average)
+            self.start_date = find_start(srs, window_start, window_end, ma_window=moving_average)
         else:
-            start = window_start
+            self.start_date = window_start
             
-        end = find_end_baseline(srs, start, window_end, threshold=recovery_threshold)
-        sarimax = SARIMAX_predictor(srs, start, end, sarimax_params)
+        self.end_date = find_end_baseline(srs, self.start_date, window_end, threshold=recovery_threshold)
+        sarimax = SARIMAX_predictor(srs, self.start_date, self.end_date, sarimax_params)
         self.predicted = sarimax
-        return calc_resid(srs, sarimax, start, end)
+        return calc_resid(srs, sarimax, self.start_date, self.end_date)
 
-    def graph(self, window_start=None, window_end=None, sarimax_params=(5, 1, 1), standardize=False, **kwargs):
+    def graph(self, window_start=None, window_end=None, sarimax_params=(5, 1, 1), standardize=False, recovery_threshold=0.9, **kwargs):
         """
         Graphs series along with forecasted trendline starting at recession and ending at end of series.
 
@@ -95,24 +97,31 @@ class Sigmet:
             srs = standardize(srs)
 
         if window_start == None or window_end == None:
-            plt.plot(srs)
-            plt.xlabel('Time')
+            warnings.warn(
+                UserWarning(
+                    'Plotting series with forecast in default class window. Call fit() with different window_start and window_end values to change the window range.'
+                )
+            )
+            start = self.start_date
+            end = self.end_date
+            sarimax = self.predicted
+        else:
+            start = find_start(srs, window_start, window_end)
+            end = find_end_baseline(srs, start, window_end, threshold=recovery_threshold)
+            sarimax = SARIMAX_predictor(srs, self.start_date, self.end_date, sarimax_params)
 
-        start = find_start(srs, window_start, window_end)
-        sarimax = SARIMAX_predictor(srs, start, sarimax_params)
-        forecasted = srs[srs.index <= start].append(sarimax)
-        dy = forecasted - srs
-
+        recession = srs.loc[(srs.index <= end) & (srs.index >= start)]
+        sarimax = pd.concat([srs.loc[(srs.index == start)], sarimax])
         fig, ax = plt.subplots(2)
         ax[0].plot(srs)
-        ax[0].plot(forecasted)
+        ax[0].plot(sarimax)
         ax[0].set_xlabel("Time")
 
         ax[1].plot(srs)
-        ax[1].scatter(x=srs.index, y=srs)
-        ax[1].plot(forecasted)
-        ax[1].scatter(x=srs.index, y=forecasted)
-        ax[1].fill_between(x=srs.index, y1=srs, y2=forecasted, alpha=0.3, color='gray')
-        ax[1].vlines(x=srs.index, ymin=srs, ymax=srs + dy)
+        # ax[1].scatter(x=srs.index, y=srs)
+        ax[1].plot(sarimax)
+        # ax[1].scatter(x=srs.index, y=forecasted
+        ax[1].fill_between(x=recession.index, y1=recession, y2=sarimax, alpha=0.3, color='gray')
+        ax[1].vlines(x=recession.index, ymin=recession, ymax=sarimax)
         ax[1].set_xlabel("Time")
         return fig
